bbe7b36a6f364fddfc9d2b97721fe17cff42db6c
3picF4iL <haskypl1@gmail.com>
2020-12-03 22:01:35 +0100

    Reorganize code, add if state checking win or points add. Debug lines still there. No collision method implemented

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ from interface_creator import InterfaceCreator
 from misc_functions import clsrc
 from msvcrt import getch
 
-
 _map = Map()  # map object created
-_map.generate_boards(10)  # generating map with argument corresponded to the desired map size
+_map.generate_boards(10, "k", 11)   # generating map with argument corresponded to the desired map size,
+                                    # symbol to collect, amount of the symbols
 interface = InterfaceCreator(_map.size_map_od)  # generating interface layout depends of the map size
 target_map = _map.generated_map  # creating variable target_map that can be used to make changes by players
 
@@ -39,7 +39,18 @@ while not end_game:
     if get_key is 27:
         break
     moving[get_key](target_map)
+    # if player_1.collision(player_1.x, player_1.y, player_2.x, player_2.y):
+    #     print("Collision")
+    #     get_key2 = ord(getch())
+    interface.p1Points = player_1.points
+    interface.p2Points = player_2.points
+    if interface.check_score(_map.symbol_amount):
+        interface.check_win()
+        target_map = _map.generate_boards(10, "k", 11)
+        player_1.respawn(target_map)
+        player_2.respawn(target_map)
 
-
+    print(f"Position X player 2: {player_2.x}\nPosition Y player 2: {player_2.y}")
+    print(f"Position X player 1: {player_1.x}\nPosition Y player 1: {player_1.y}")
 
 
