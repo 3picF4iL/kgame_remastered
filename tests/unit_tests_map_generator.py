2f07444b886f1576882da269d70d05588b191d59
3picF4iL <haskypl1@gmail.com>
2020-12-07 16:52:53 +0100

    Unit tests for Map Generator

---
+++ tests/unit_tests_map_generator.py
@@ -0,0 +1,20 @@
+# import unittest
+# from map_generator import Map
+#
+#
+# class TestMapGenerator(unittest.TestCase):
+#
+#     #def test_add_item(self):
+#
+#     def test_neighbor(self):
+#         pass
+#
+#     def test_add_item(self):
+#         self.assertIn(self, "k", )
+#
+#     def tearDown(self):
+#         pass
+#
+#
+# if __name__ == '__main__':
+#     unittest.main()
